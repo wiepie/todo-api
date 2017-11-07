e424e054014172e99c290c7857258b9f354d2637
pelle <wiepiegames1@gmail.com>
2017-11-07 08:14:51 +0100

    added update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,25 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server')
+
+  db.collection('Users').findOneAndUpdate({
+    name: "Mike"
+  }, {
+    $set: {
+      name: "Pelle"
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  // db.close();
+});
